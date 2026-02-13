29e8822d52e4fb33b16172e86531a7e34e474003
Iagobarros211256 <iagobarrosgomes2112@gmail.com>
2026-02-13 20:55:42 -0300

    feat: support DATABASE_URL for cloud deploy

--- db/db.go
+++ db/db.go
@@ -2,23 +2,42 @@ package db
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
 func SetupDB() *sql.DB {
+	databaseURL := os.Getenv("DATABASE_URL")
+
+	// Se existir DATABASE_URL (Render)
+	if databaseURL != "" {
+		db, err := sql.Open("postgres", databaseURL)
+		if err != nil {
+			log.Fatalf("failed to open db connection: %v", err)
+		}
+
+		if err := db.Ping(); err != nil {
+			log.Fatalf("failed to connect to db: %v", err)
+		}
+
+		return db
+	}
+
+	// Fallback para ambiente local (Docker)
 	host := getEnv("DB_HOST", "localhost")
 	port := getEnv("DB_PORT", "5432")
 	user := getEnv("DB_USER", "postgres")
 	password := getEnv("DB_PASSWORD", "postgres")
 	dbname := getEnv("DB_NAME", "volurya")
 
-	dsn := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname,
-	)
+	dsn := "host=" + host +
+		" port=" + port +
+		" user=" + user +
+		" password=" + password +
+		" dbname=" + dbname +
+		" sslmode=disable"
 
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
