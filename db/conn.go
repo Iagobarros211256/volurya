a6067ef9ef162663a5ac30687df0ab22359d0c5d
Iagobarros211256 <iagobarrosgomes2112@gmail.com>
2026-02-13 21:06:44 -0300

    fixing some database issues to deploy on render

--- db/conn.go
+++ db/conn.go
@@ -2,42 +2,33 @@ package db
 
 import (
 	"database/sql"
-	"fmt"
-	"time"
+	"log"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	host     = "volurya_postgres"
-	port     = 5432
-	user     = "volurya"
-	password = "volurya"
-	dbname   = "volurya_db"
-)
-
 func ConnectDB() (*sql.DB, error) {
-	psqlInfo := fmt.Sprintf(
-		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname,
-	)
+	databaseURL := os.Getenv("DATABASE_URL")
 
-	var db *sql.DB
-	var err error
-
-	for attempt := 1; attempt <= 10; attempt++ {
-		db, err = sql.Open("postgres", psqlInfo)
-		if err == nil {
-			err = db.Ping()
-			if err == nil {
-				fmt.Println("Connected to " + dbname)
-				return db, nil
-			}
-		}
-
-		fmt.Printf("Database not ready (attempt %d), retrying...\n", attempt)
-		time.Sleep(2 * time.Second)
+	if databaseURL == "" {
+		return nil, logError("DATABASE_URL not set")
 	}
 
-	return nil, fmt.Errorf("could not connect to database after retries: %w", err)
+	db, err := sql.Open("postgres", databaseURL)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	log.Println("Connected to database successfully")
+	return db, nil
+}
+
+func logError(msg string) error {
+	log.Println(msg)
+	return nil
 }
