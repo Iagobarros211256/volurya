1fb25c3044831d38a79df64dcf318d9fe45d6170
Iagobarros211256 <iagobarrosgomes2112@gmail.com>
2026-04-30 04:16:03 -0300

    changed to use absolute path

--- db/migrate.go
+++ db/migrate.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
@@ -16,8 +18,16 @@ func RunMigrations(db *sql.DB) error {
 		return fmt.Errorf("failed to create migration driver: %w", err)
 	}
 
+	// Busca o diretório do executável
+	execPath, err := os.Executable()
+	if err != nil {
+		return fmt.Errorf("failed to get executable path: %w", err)
+	}
+	execDir := filepath.Dir(execPath)
+	migrationsPath := filepath.Join(execDir, "db", "migrations")
+
 	m, err := migrate.NewWithDatabaseInstance(
-		"file://db/migrations",
+		"file://"+migrationsPath,
 		"postgres",
 		driver,
 	)
